b62d6ab5117fe9cea4c8fc1baca64285954df5da
Jan Ripke <janripke@gmail.com>
2020-12-19 22:23:28 +0100

    setting the development version

--- paprika_connector/__init__.py
+++ paprika_connector/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1-dev0'
 
 __title__ = 'paprika-connector'
 __description__ = 'paprika-connector for connecting to databases using the repository concept'
