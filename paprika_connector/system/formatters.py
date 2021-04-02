eb8237df5da9f5533ccec7ff9b8faf73a02c91d2
Jan Ripke <janripke@gmail.com>
2021-04-02 16:32:21 +0200

    added values formatter

--- paprika_connector/system/formatters.py
+++ paprika_connector/system/formatters.py
@@ -7,3 +7,18 @@ def set_formatter(dict_, excludes):
                 result += '{}=:{}, '.format(key, key)
         result = result.rstrip(', ')
     return result
+
+
+def values_formatter(dict_, excludes=[], ignore_none=True):
+    columns = []
+    values = []
+    if dict_:
+        keys = dict_.keys()
+        for key in keys:
+            if key not in excludes:
+                if not ignore_none or dict_[key]:
+                    columns.append(f'{key}')
+                    values.append(f':{key}')
+        columns = ",".join(columns)
+        values = ",".join(values)
+        return f'({columns}) values ({values})'
